cbadfe0c2fde1a5600a8f45b214e5ed41a0378a1
madualex3 <ogochukwumadu.ng@gmail.com>
2024-02-14 13:55:57 +0100

    created pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Listings from './pages/Listings'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
 
 const App = () => {
   return (
-    <div className="text-3xl text-red-500">APPPPP</div>
+    <BrowserRouter>
+      <Routes>
+        <Route index exact element={<Home />} />
+        <Route path='/listings' element={<Listings />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
